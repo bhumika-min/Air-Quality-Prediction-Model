e1a26380593734fd54c310164d73f7511bcf36a1
bhumika-min <135568939+bhumika-min@users.noreply.github.com>
2025-05-18 22:19:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -16,5 +16,7 @@ def index():
         prediction = round(pred, 2)
     return render_template("index.html", prediction=prediction)
 
-if __name__ == "__main__":
-    app.run(debug=True)
+
+if __name__ == '__main__':
+    from os import environ
+    app.run(host='0.0.0.0', port=int(environ.get("PORT", 5000)))
